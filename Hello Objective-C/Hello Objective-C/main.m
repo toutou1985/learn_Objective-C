10566816f6ca50913c43458102f9926d3163a3fb
toutou <haitaoliu1985@gmail.com>
2015-09-02 23:55:35 +0900

    gitignore

---
+++ Hello Objective-C/Hello Objective-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello Objective-C
+//
+//  Created by 劉海涛 on 2015/09/02.
+//  Copyright (c) 2015年 monstar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
